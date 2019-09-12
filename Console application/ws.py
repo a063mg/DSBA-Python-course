8310a0d1777b3cb38d02cae39b20aa69ab68bf84
a007mg <alekseev.artem700@gmail.com>
2019-09-12 18:31:04 +0300

    Ok
    Ok

---
+++ Console application/ws.py
@@ -0,0 +1,26 @@
+import os, time
+import argparse
+import datetime
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-m", "--mtime", help="Print the time of last file modification", choices=['1', '2', '3'])
+
+parser.add_argument("-s", "--size", help="Print the size of file in MB", action="store_true")
+
+# parser.add_argument("--rename", help="Change file name to B", action="store", type=str, default="hw", nargs="=", metavar=None)
+
+parser.add_argument("--rename", help="Something")
+
+args = parser.parse_args()
+
+file = input()
+
+st = os.stat(file)
+
+	print("last modified: %s" % time.ctime(st.st_mtime), args.mtime)
+if args.size:
+	print("the size of file is: %s" % (st.st_size/2**20),"MB")
+if args.rename:
+	os.rename(file, args.rename)
+	print("Renamed successfully")
